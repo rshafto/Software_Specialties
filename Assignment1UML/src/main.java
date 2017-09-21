e2ee110b3b1d30870423e8038498ae57519735f7
Christian Fusco <christian.fusco@mymail.champlain.edu>
2017-09-21 15:54:12 -0400

    Header info for assignment 1

--- Assignment1UML/src/main.java
+++ Assignment1UML/src/main.java
@@ -1,3 +1,22 @@
+"""Author: Brian Zhang and Christian Fusco
+Class: CSI-480-01/02
+Assignment: Lab 1
+Date Assigned: 9/12/2017
+Due Date: 9/19/2017
+
+Description:
+UML Design doc with example java code.
+
+Certification of Authenticity: 
+I certify that this is entirely my own work, except where I have given 
+fully-documented references to the work of others. I understand the definition 
+and consequences of plagiarism and acknowledge that the assessor of this 
+assignment may, for the purpose of assessing this assignment:
+- Reproduce this assignment and provide a copy to another member of academic
+- staff; and/or Communicate a copy of this assignment to a plagiarism checking
+- service (which may then retain a copy of this assignment on its database for
+- the purpose of future plagiarism checking)
+"""
 
 public class main {
 	public static void main(String[] args) {
